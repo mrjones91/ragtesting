fc5137639d276cdd83128b126a06819bd6ad7a6a
DJ <epicmrjones@gmail.com>
2024-11-16 17:37:43 -0600

    RAG can query Database and provide specific responses about the overall or specific data.

--- v4/index.ts
+++ v4/index.ts
@@ -1,9 +1,23 @@
-import { RAGApplicationBuilder } from '@llm-tools/embedjs';
+import { JsonLoader, RAGApplicationBuilder } from '@llm-tools/embedjs';
 import { Ollama, OllamaEmbeddings } from '@llm-tools/embedjs-ollama';
 import { WebLoader } from '@llm-tools/embedjs-loader-web';
 import { PdfLoader } from '@llm-tools/embedjs-loader-pdf';
-import { HNSWDb } from '@llm-tools/embedjs-hnswlib';
+import { CsvLoader } from '@llm-tools/embedjs-loader-csv';
+import { MongoDb } from '@llm-tools/embedjs-mongodb';
+
 import express, { Request, Response} from "express";
+
+import { MongoClient } from 'mongodb';
+
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const dbuser = process.env.dbUsername;
+const dbpwd = process.env.dbPwd;
+const dbconn = process.env.dbConnection || "";
+
+// console.log(`debug\n${dbuser}\n${dbpwd}\n${dbconn}`);
 
 const app = express();
 const port = 8080;
@@ -13,18 +27,22 @@ app.use(express.json());
 const ragApplication = await new RAGApplicationBuilder()
 .setModel(new Ollama({ modelName: "llama3.2", baseUrl: 'http://localhost:11434' }))
 .setEmbeddingModel(new OllamaEmbeddings({ model: 'nomic-embed-text', baseUrl: 'http://localhost:11434' }))
-.setVectorDatabase(new HNSWDb())
+.setVectorDatabase(new MongoDb({
+    connectionString: `mongodb+srv://${dbuser}:${dbpwd}@cluster0.zep0qmk.mongodb.net/BlightBounties`,
+}))
 .build();
 
 const Loaders = { 
     'pdf': [PdfLoader, 'filePathOrUrl'],
-    'web': [WebLoader, 'urlOrContent']
+    'web': [WebLoader, 'urlOrContent'],
+    'csv': [CsvLoader, 'filePathOrUrl']
 }
 const contentName = {
 
 }
 // let pdf = await ragApplication.addLoader(new PdfLoader({ filePathOrUrl: 'https://bitcoin.org/bitcoin.pdf' }))
 // let web = await ragApplication.addLoader(new WebLoader({ urlOrContent: 'https://bitcoin.org/bitcoin.pdf' }))
+// let csv = await ragApplication.addLoader(new CsvLoader({ filePathOrUrl: './Service_Requests_since_2016_20241116.csv' }))
 
 const greetings = "Hello via Bun!";
 
@@ -43,6 +61,18 @@ app.post('/ask', async (req: Request, res: Response) => {
         } else if (source.type == 'web') {
             await ragApplication.addLoader(new WebLoader({ urlOrContent: source.link }))
 
+        } else if (source.type == 'csv') {
+            await ragApplication.addLoader(new CsvLoader({ filePathOrUrl: source.link }))
+        } else if (source.type == 'json') {
+            let client = new MongoClient(dbconn);
+            await client.connect();
+            console.log('Connected successfully to server');
+            const db = client.db('BlightBounties');
+            const collection = db.collection('blight');
+            const results = await collection.aggregate([{$sort: { CREATION_DATE: -1 }}]).limit(20).toArray();
+            await ragApplication.addLoader(new JsonLoader({object: results}));
+            console.log('resource loaded: ');
+            console.log(results);
         }
     });
     const result = await ragApplication.query(req.body.query);
